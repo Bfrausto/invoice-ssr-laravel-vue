dc269c2711dba850e5db6c27a2ca53f812950abc
Brian Frausto <brian@hireline.io>
2025-08-20 19:05:35 -0600

    feat(database): define initial schema and models

---
+++ app/Models/Invoice.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Invoice extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'company_id',
+        'client_id',
+        'invoice_number',
+        'date',
+        'due_date',
+        'total_amount',
+        'status',
+        'issue_date',
+        'tax_id',
+        'folio',
+        'subtotal',
+        'total_taxes',
+        'total',
+        'notes',
+    ];
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    public function client()
+    {
+        return $this->belongsTo(Client::class);
+    }
+
+    public function tax()
+    {
+        return $this->belongsTo(Tax::class);
+    }
+
+    public function items()
+    {
+        return $this->hasMany(InvoiceItem::class);
+    }
+}
